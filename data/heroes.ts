251f8b18de474cc5270d6ed3b4a9cb28366b2892
Erike Nascimento <Nascimento.lf.erike@gmail.com>
2026-02-19 18:25:20 -0300

    starting the populating of the hero type

---
+++ data/heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from "@/types/hero"
+
+export const HEROES: Hero[] = [
+  {
+    id: "abrams",
+    name: "Abrams",
+    description: ["Hard hitting, hard headed, and hard drinking; Detective Abrams has been a fixture on the New York investigating scene for years. From stolen art, to missing persons, to ritual murders; Abrams didn't just take any case that came across his desk... he solved them.","But his days of following cheating spouses came to an end the day he opened his office door and found The Tome sitting on his desk.", "No instruction was left for him save for a brief note scrawled in onyx blood that read \"Don't let them have it\" Abrams hasn't figured where the thing came from; but seeing as his home has been broken into, his office tossed on 3 different occasions, and his car firebombed he has a vested interest in figuring out what the hell is going on."],
+    tags: ["Tank", "Brawler", "Bull-Headed"],
+  },
+  {
+    id: "apollo",
+    name: "Apollo",
+    description: ["A land of beauty, tradition and conflict; North Ixia is no stranger to brutal political violence. So when Apollo was told that it was for his own safety he was being sent to New York for school, he knew the situation was serious.", "Adjusting to life at Blackmore Academy didn’t come easy to Apollo. His school was supposed to be filled with The Cursed Apple’s best and brightest but all he saw, with few exceptions, was a sea of aggressive mediocrity. Electing not to waste his time with frivolous things like parties and dances, he instead put his energy on the piste, where he rigorously trains as captain of the Blackmore fencing team.", "However, at the end of the day Apollo does not fret too much on the tedium of life in New York. He knows that eventually the time will come when his father will call on him to come home and he will get to show that he is worthy of wielding his family's ancestral blade."],
+    tags: ["Finesse", "Mobility", "A Cut Above"],
+  },
+  /*
+  {
+    id: "",
+    name: "",
+    description: ["", "", ""],
+    tags: ["", "", ""],
+  },
+  */
+]
