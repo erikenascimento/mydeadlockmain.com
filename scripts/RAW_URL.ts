38ffd4be3e18b4660cef43cdf16ddf61d244e1d6
Erike Nascimento <Nascimento.lf.erike@gmail.com>
2026-02-23 19:54:18 -0300

    placing the URL in a separate file for reuse later

---
+++ scripts/RAW_URL.ts
@@ -0,0 +1,2 @@
+export const RAW_URL: String =
+  "https://raw.githubusercontent.com/deadlock-api/deadlock-api-assets/master/res/abilities.vdata"
--- scripts/download-abilities.ts
+++ scripts/download-abilities.ts
@@ -1,15 +1,14 @@
-const RAW_URL =
-  "https://raw.githubusercontent.com/deadlock-api/deadlock-api-assets/master/res/abilities.vdata"
+import { RAW_URL } from "./RAW_URL"
 
 async function main() {
-  const res = await fetch(RAW_URL)
+    const res = await fetch(`${RAW_URL}`)
 
   if (!res.ok) {
     throw new Error(`Failed to download: ${res.status}`)
   }
 
   const text = await res.text()
-  console.log("🚀 ~ main ~ text:", text)
+  console.log(text.slice(0, 500))
 }
 
 main()
