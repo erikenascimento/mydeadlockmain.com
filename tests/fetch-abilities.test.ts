cc9643b72f708b522bdfa02b8fe3bc0f4fa57cc5
Erike Nascimento <Nascimento.lf.erike@gmail.com>
2026-03-02 16:30:56 -0300

    defining a more robust regex for the check

--- tests/fetch-abilities.test.ts
+++ tests/fetch-abilities.test.ts
@@ -3,12 +3,9 @@ import assert from "node:assert";
 import { fetchAbilities } from "@/scripts/fetch/fetch-abilities"
 
 test("Confirm the file is a KV3 by checking its head", async () => {
-    const resultString = await fetchAbilities();
+    const responseBody = await fetchAbilities();
 
-    console.log("Head:", resultString.slice(0, 133));
+    console.log("Head:", responseBody.slice(0, 133));
 
-    assert.ok(
-        resultString.startsWith("<!--"),
-        "Response does not start with '<!--'"
-    );
+    assert.match(responseBody, /^<!--\s*kv3[^]*-->/i);
 });
